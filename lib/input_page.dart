dea98afa17b32771ab726e0991e060c7a8d4a421
tivorlu <gulceninmesenesi@hotmail.com>
2021-12-10 22:51:23 +0300

    RotatedBox+Slider

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -3,22 +3,23 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 import 'mycolumn.dart';
 import 'mycontainer.dart';
-class InputPage extends StatefulWidget {
 
+class InputPage extends StatefulWidget {
   @override
-  _InputPageState createState() => _InputPageState() ;
+  _InputPageState createState() => _InputPageState();
 }
 
 class _InputPageState extends State<InputPage> {
   String? seciliCinsiyet;
+  double icilenSigara = 0.0;
+  double sporYapilanGun = 0.0;
 
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
       appBar: AppBar(
-        title:
-        Text(
+        title: Text(
           'YAŞAM BEKLENTİSİ',
           style: TextStyle(
             color: Colors.black,
@@ -28,67 +29,142 @@ class _InputPageState extends State<InputPage> {
         centerTitle: true,
       ),
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
           Expanded(
-            child:
-                Row(
-                  children: [
-                    Expanded(
-                      child: MyContainer(child: Column()),
-                    ),
-                    Expanded(
-                      child: MyContainer(child: Column())
-                    ),
-                  ],
-                ),
-
-              ),
-          Expanded(
-            child:
-            MyContainer(child: Row())
-          ),
-          Expanded(
-            child:
-            MyContainer(child: Row())
-          ),
-          Expanded(
-            child:
-            Row(
+            child: Row(
               children: [
                 Expanded(
-                  child: GestureDetector(
-                    onTap: (){
+                  child: MyContainer(
+                    child: Row(
+                      children: [
+                        new RotatedBox(
+                          quarterTurns: 3,
+                          child: Text(
+                            'BOY',
+                            style: TextStyle(
+                              color: Colors.lightBlue,
+                              fontSize: 18,
+                            ),
+                          ),
+                        ),
+                        new RotatedBox(
+                          quarterTurns: 3,
+                          child: Text(
+                            '170',
+                            style: TextStyle(
+                              color: Colors.lightBlue,
+                              fontSize: 18,
+                            ),
+                          ),
+                        ),
+                        Column(),
+                      ],
+                    ),
+                  ),
+                ),
+                Expanded(child: MyContainer(child: Column())),
+              ],
+            ),
+          ),
+          Expanded(
+              child: MyContainer(
+                  child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Haftada Kaç Gün Spor Yapıyorsunuz?',
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 10),
+              Text(
+                '${sporYapilanGun.round()}',
+                style: TextStyle(
+                  color: Colors.lightBlue,
+                  fontSize: 18,
+                ),
+              ),
+              Slider(
+                  min: 0.0,
+                  max: 7.0,
+                  divisions: 7,
+                  value: sporYapilanGun,
+                  onChanged: (double newValue) {
+                    setState(() {
+                      sporYapilanGun = newValue;
+                    });
+                  })
+            ],
+          ))),
+          Expanded(
+              child: MyContainer(
+                  child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Günde Kaç Sigara İçiyorsunuz?',
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 10),
+              Text(
+                '${icilenSigara.round()}',
+                style: TextStyle(
+                  color: Colors.lightBlue,
+                  fontSize: 18,
+                ),
+              ),
+              Slider(
+                  min: 0,
+                  max: 30,
+                  divisions: 30,
+                  value: icilenSigara,
+                  onChanged: (double newValue) {
+                    setState(() {
+                      icilenSigara = newValue;
+                    });
+                  })
+            ],
+          ))),
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(
+                  child: MyContainer(
+                    renk:
+                        seciliCinsiyet == 'KADIN' ? Colors.green : Colors.white,
+                    onPressxx: () {
                       setState(() {
                         seciliCinsiyet = 'KADIN';
                       });
-
                     },
-                    child: MyContainer(
-                      renk: seciliCinsiyet=='KADIN'?Colors.purple : Colors.black26,
-                        child: MyColumn(cinsiyet: 'KADIN', ikon: FontAwesomeIcons.venus),
-                    ),
+                    child: MyColumn(
+                        cinsiyet: 'KADIN', ikon: FontAwesomeIcons.venus),
                   ),
                 ),
                 Expanded(
-                  child: GestureDetector(
-                    onTap: (){
-                      setState(() {
-                        seciliCinsiyet = 'ERKEK';
-                      });
-                    },
-                    child: MyContainer(
-                      renk: seciliCinsiyet=='ERKEK'?Colors.green : Colors.white,
-                      child: MyColumn(cinsiyet: 'ERKEK', ikon: FontAwesomeIcons.mars)
-                    ),
-                  ),
+                  child: MyContainer(
+                      onPressxx: () {
+                        setState(() {
+                          seciliCinsiyet = 'ERKEK';
+                        });
+                      },
+                      renk: seciliCinsiyet == 'ERKEK'
+                          ? Colors.green
+                          : Colors.white,
+                      child: MyColumn(
+                          cinsiyet: 'ERKEK', ikon: FontAwesomeIcons.mars)),
                 ),
               ],
             ),
           ),
-            ],
-          ),
-        );
+        ],
+      ),
+    );
   }
 }
-
-
--- lib/mycontainer.dart
+++ lib/mycontainer.dart
@@ -1,20 +1,26 @@
+import 'dart:ffi';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class MyContainer extends StatelessWidget {
   final Color renk;
   final Widget child;
+  final Function()? onPressxx;
 
-  MyContainer ({this.renk = Colors.white , required this.child }); //renk yazılmadıysa default rengi beyaz olsun demiş olduk.
+  MyContainer ({this.renk = Colors.white , required this.child, this.onPressxx = null}); //renk yazılmadıysa default rengi beyaz olsun demiş olduk.
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: child,
-      margin: EdgeInsets.all(12.0),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(10.0),
-        color: renk,
+    return GestureDetector(
+      onTap: onPressxx,
+      child: Container(
+        child: child,
+        margin: EdgeInsets.all(12.0),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10.0),
+          color: renk,
+        ),
       ),
     );
   }
